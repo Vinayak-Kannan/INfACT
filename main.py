9d1247f65fd2697f9f68fbdac5c7a87e37c9ef75
vinayakkannan <vk2364@columbia.edu>
2023-09-18 11:06:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+from SupportingFunction.ParseData import parse_scraped_data
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def driver():
+    # Use a breakpoint in the code line below to debug your script.
+    parse_scraped_data()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    driver()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
